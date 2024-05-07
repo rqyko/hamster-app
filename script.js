bca900344626a13ecd47cebc79a00ac682109601
rqyko <reyko.boland@student.graafschapcollege.nl>
2024-05-07 13:33:40 +0200

    Fix / Load hamster

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function closeSheet() {
   document.querySelector(".sheet").classList.add("sheet-out-of-view");
   document.querySelector("body").style.overflowY = "auto";
 
-  history.pushState(null, "", "/");
+  history.pushState(null, "", "https://net24rboland.gc-webhosting.nl/");
 }
 
 document.querySelector("#close-button").addEventListener("click", () => {
